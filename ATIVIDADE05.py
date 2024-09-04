45911226fab95f95cda120e214c25c2c926bfe75
MateusMorais796 <moraiscosta321@gmail.com>
2024-09-04 16:02:40 -0300

    conversão de minutos para segundo

---
+++ ATIVIDADE05.py
@@ -0,0 +1,6 @@
+# Escreva um programa que receba um valor em minutos e converta para segundos.
+minutos = float(input('digite o valor em minutos '))
+
+conversao = minutos * 60
+
+print(f'a conversão de minutos para segundos é {conversao}')
